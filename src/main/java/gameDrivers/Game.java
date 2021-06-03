666968b9f713d994524133b0f658e987cc47a4b1
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-06-04 00:40:04 +0200

    :sparkles: add api

---
+++ src/main/java/gameDrivers/Game.java
@@ -0,0 +1,17 @@
+package gameDrivers;
+
+public class Game {
+    private int numOfPegs = 4;
+    private int numOfRows = 10;
+    private int numOfColors = 4;
+
+    public static void main(String[] args) {
+
+    }
+
+    public Game() {
+//        init game
+    }
+
+
+}
---
+++ src/main/java/gameDrivers/Main.java
@@ -0,0 +1,20 @@
+package gameDrivers;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     *
+     *
+     * @return true if user_1 won else false
+     */
+    public boolean initNewRow()  {
+
+    }
+
+//    todo return num of correct positions and colors
+
+}
