1a80c8f9a5abbea9a3241b3193b706c4f61c5395
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-06-04 03:15:36 +0200

    :recycle: cleanup utils

--- src/main/java/civare/mastermind/resourceManagers/constants/Utils.java
+++ src/main/java/civare/mastermind/resourceManagers/constants/Utils.java
@@ -5,11 +5,9 @@ package civare.mastermind.resourceManagers.constants;
 
 import civare.mastermind.Main;
 
-import javax.swing.*;
 import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Scanner;
 
 public class Utils {
 	// get a file from the resources folder
@@ -27,34 +25,6 @@ public class Utils {
 			return inputStream;
 		}
 
-	}
-
-	public static void copyContent(File a, File b)
-			throws Exception {
-
-		try {
-			FileInputStream in = new FileInputStream(a);
-		} catch (Exception e ) {
-			e.printStackTrace();
-		}
-
-		try (FileInputStream in = new FileInputStream(a); FileOutputStream out = new FileOutputStream(b)) {
-
-			int n;
-
-			// read() function to read the
-			// byte of data
-			while ((n = in.read()) != -1) {
-				// write() function to write
-				// the byte of data
-				out.write(n);
-			}
-		}
-		// close() function to close the
-		// stream
-		// close() function to close
-		// the stream
-		System.out.println("File Copied");
 	}
 
 	public static class CopyFileVisitor extends SimpleFileVisitor<Path> {
@@ -85,78 +55,52 @@ public class Utils {
 		}
 	}
 
-	public  class Temp {
-		public Temp() {
-
+	/**
+	 * creates file if not present
+	 * checks if source file is not present and returns false
+	 *
+	 * @param sourcePath
+	 * @param targetPath
+	 */
+	public static boolean copyContentOfFolder(Path sourcePath, Path targetPath) {
+		if (! sourcePath.toFile().exists()) {
+			System.out.println("file "+ sourcePath+" does not exist");
+			return false;
 		}
+
+
+		try {
+			Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult preVisitDirectory(final Path dir,
+														 final BasicFileAttributes attrs) throws IOException {
+					Files.createDirectories(targetPath.resolve(sourcePath
+							.relativize(dir)));
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFile(final Path file,
+												 final BasicFileAttributes attrs) throws IOException {
+					Files.copy(file,
+							targetPath.resolve(sourcePath.relativize(file)));
+					return FileVisitResult.CONTINUE;
+				}
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 	}
 
 	public static void main(String[] args) throws Exception {
-		final Path targetPath = Paths.get("C:/b");
 		final Path sourcePath = Paths.get("C:/a");
-				Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
-					@Override
-					public FileVisitResult preVisitDirectory(final Path dir,
-															 final BasicFileAttributes attrs) throws IOException {
-						Files.createDirectories(targetPath.resolve(sourcePath
-								.relativize(dir)));
-						return FileVisitResult.CONTINUE;
-					}
+		final Path targetPath = Paths.get("C:/b");
+		copyContentOfFolder(sourcePath, targetPath);
 
-					@Override
-					public FileVisitResult visitFile(final Path file,
-													 final BasicFileAttributes attrs) throws IOException {
-						Files.copy(file,
-								targetPath.resolve(sourcePath.relativize(file)));
-						return FileVisitResult.CONTINUE;
-					}
-				});
-
-//		Files.walkFileTree("C:/a", new CopyFileVisitor(new Path(String.valueOf(getFileFromResourceAsStream("C:/b")))));
-
-
-		//		String source = "C:/your/source";
-//		File srcDir = new File(source);
-//
-//		String destination = "C:/your/destination";
-//		File destDir = new File(destination);
-//
-//		try {
-//			FileUtils.copyDirectory(srcDir, destDir);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-
-//
-//		Scanner sc = new Scanner(System.in);
-//
-//		// get the source file name
-//		System.out.println(
-//				"Enter the source filename from where you have to read/copy :");
-////		String a = sc.nextLine();
-
-//		JFileChooser fileChooser = new JFileChooser();
-//		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-//		int result = fileChooser.showOpenDialog(this);
-//		if (result == JFileChooser.APPROVE_OPTION) {
-//			File selectedFile = fileChooser.getSelectedFile();
-//			System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-//		}
-//		// source file
-//	File x = new File("C:/a/");
-//		System.out.println(x.exists());
-//		// get the destination file name
-//		System.out.println(
-//				"Enter the destination filename where you have to write/paste :");
-////		String b = sc.nextLine();
-//
-//		// destination file
-//		File y = new File("C:\\b");
-//		System.out.println(y.exists());
-//
-//		// method called to copy the
-		// contents from x to y
-//		copyContent(x, y);
 	}
 }
 
