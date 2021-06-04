e72531ad5c6c8160554e4fa310de0bcd1c240914
Ivan Joskic <i.joskic@outlook.com>
2021-06-04 02:18:38 +0200

    Add function to check game state
    Also randomly generate final state.

--- src/main/java/gameDrivers/Game.java
+++ src/main/java/gameDrivers/Game.java
@@ -1,17 +1,29 @@
 package gameDrivers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
-    private int numOfPegs = 4;
-    private int numOfRows = 10;
-    private int numOfColors = 4;
+	private int codeLength = 4; // 4, 6 or 8
+	private int numOfRows = 10; // 6, 8, 10 or 12
+	private int numOfColors = 6;
+	private List<Object> finalState;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    }
+	}
 
-    public Game() {
+	public Game() {
 //        init game
-    }
+
+		// generate random final state of the board
+		this.finalState = new ArrayList<>();
+		for (int i = 0; i < codeLength; i++) {
+			this.finalState.add((Math.random() * 100) % numOfColors);
+		}
+
+		//
+	}
 
 
 }
--- src/main/java/gameDrivers/Main.java
+++ src/main/java/gameDrivers/Main.java
@@ -1,19 +1,43 @@
 package gameDrivers;
 
+import java.util.List;
+import java.util.TreeSet;
+
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    }
+	}
 
-    /**
-     *
-     *
-     *
-     * @return true if user_1 won else false
-     */
-    public boolean initNewRow()  {
+	/**
+	 *
+	 *
+	 *
+	 * @return true if user_1 won else false
+	 * TODO move finalState to somewhere else
+	 */
+	public boolean checkGameState(List<Object> pegs, List<Object> finalState)  {
+		/*
+		 *  2 - in the right place
+		 *  1 - right but in the wrong place
+		 *  0 - wrong
+		 * */
+		TreeSet<Integer> pegInfo = new TreeSet<>(); // Set beacuse it should be sorted when displayed
+		for (int i = 0; i < pegs.size(); i++) {
+			if (pegs.get(i) == finalState.get(i)) {
+				// its in the right spot
+				pegInfo.add(2);
+			} else if (finalState.contains(pegs.get(i))) { // TODO complexity! Set.contains() = O(1)
+				// its right but in the wrong spot
+				pegInfo.add(1);
+			} else {
+				// its wrong
+				pegInfo.add(0);
+			}
+		}
+		// TODO pegInfo has to be displayed
 
-    }
+		return !(pegInfo.contains(0) || pegInfo.contains(2));
+	}
 
 //    todo return num of correct positions and colors
 
